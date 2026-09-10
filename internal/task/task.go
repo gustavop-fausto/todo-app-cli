3a13406696cacf5dd7e53fae9bbe0c581fe1f068
gustavop-fausto <gugapf08@gmail.com>
2026-09-10 20:48:56 -0300

    add: tasks' functionalities

--- internal/task/task.go
+++ internal/task/task.go
@@ -1,9 +1,7 @@
 package task
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 )
 
 type Task struct {
@@ -11,4 +9,42 @@ type Task struct {
 	Name        string `json:"name"`
 	Done        bool   `json:"done"`
 	Description string `json:"description"`
+}
+
+func Add(tasks []Task, name, description string) []Task {
+	tasks = append(tasks, Task{ID: nextID(tasks), Name: name, Done: false, Description: description})
+	return tasks
+}
+
+func Remove(tasks []Task, id int) ([]Task, error) {
+	for i := range tasks {
+		if tasks[i].ID == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			return tasks, nil
+		}
+	}
+
+	return nil, fmt.Errorf("erro id não encontrado")
+}
+
+func MarkDone(tasks []Task, id int) error {
+	for i := range tasks {
+		if tasks[i].ID == id {
+			tasks[i].Done = true
+			return nil
+		}
+	}
+
+	return fmt.Errorf("erro id não encontrado")
+}
+
+func nextID(tasks []Task) int {
+	greater := 0
+	for _, value := range tasks {
+		if value.ID > greater {
+			greater = value.ID
+		}
+	}
+
+	return greater + 1
 }
