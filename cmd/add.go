f4fe8be6a85732b38eea9c8a374bc4475e02efa2
gustavop-fausto <gugapf08@gmail.com>
2026-09-14 15:56:35 -0300

    update: command usage text of add command

--- cmd/add.go
+++ cmd/add.go
@@ -11,6 +11,7 @@ import (
 var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Adding new task to the list.",
+	Args: cobra.MinimumNArgs(1),
 
 	RunE: func(cmd *cobra.Command, args []string) error {
 		tasks, err := storage.Load()
@@ -18,11 +19,17 @@ var addCmd = &cobra.Command{
 			return fmt.Errorf("erro ao carregar tasks: %w", err)
 		}
 
-		tasks = task.Add(tasks, args[0])
+		for _, value := range args {
+			tasks = task.Add(tasks, value)
+		}
 
 		err = storage.Save(tasks)
 		if err != nil {
 			return fmt.Errorf("erro ao adicionar task: %w", err)
+		}
+
+		for _, value := range args {
+			fmt.Printf("✅ Tarefa adicionada: \"%s\"\n", value)
 		}
 
 		return nil
