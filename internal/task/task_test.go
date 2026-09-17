49aea90a65df5465d7efcad96e91d41a4fdc889e
gustavop-fausto <gugapf08@gmail.com>
2026-09-16 23:05:28 -0300

    add: test cases to markAsDone command

--- internal/task/task_test.go
+++ internal/task/task_test.go
@@ -1,9 +1,9 @@
 package task
 
 import (
+	"slices"
 	"testing"
 	"time"
-	"slices"
 )
 
 func TestAdd(t *testing.T) {
@@ -57,23 +57,25 @@ func TestAdd(t *testing.T) {
 
 			// testa se o nome da task foi salvo corretamente
 			if tt.todoToAdd != newTask.Todo {
-				t.Logf("expected task %q, got %q", tt.todoToAdd, newTask.Todo)
+				t.Errorf("expected task %q, got %q", tt.todoToAdd, newTask.Todo)
 			}
 
 			// testa se o ID que ele gerou foi o correto para aquela posição
 			if tt.expectedID != newTask.ID {
-				t.Logf("expected id %d, got %d", tt.expectedID, newTask.ID)
+				t.Errorf("expected id %d, got %d", tt.expectedID, newTask.ID)
 			}
 
 			// testa se cada task inicializa como nao concluida
 			if newTask.Done != false {
-				t.Logf("expect initialize the task as not done")
+				t.Errorf("expect initialize the task as not done")
 			}
 		})
 	}
 }
 
 func TestRemove(t *testing.T) {
+	createdAt := time.Now().Add(-10 * time.Second)
+
 	tests := []struct {
 		name           string
 		tasks          []Task
@@ -93,12 +95,12 @@ func TestRemove(t *testing.T) {
 		{
 			name: "Remover uma task com um id inválido",
 			tasks: []Task{
-				{ID: 1, Todo: "Tarefa 1", Done: false, CreatedAt: time.Now().Add(-10 * time.Second)},
-				{ID: 2, Todo: "Tarefa 2", Done: false, CreatedAt: time.Now().Add(-10 * time.Second)},
+				{ID: 1, Todo: "Tarefa 1", Done: false, CreatedAt: createdAt},  
+				{ID: 2, Todo: "Tarefa 2", Done: false, CreatedAt: createdAt},
 			},
 			remainingTasks: []Task{
-				{ID: 1, Todo: "Tarefa 1", Done: false, CreatedAt: time.Now().Add(-10 * time.Second)},
-				{ID: 2, Todo: "Tarefa 2", Done: false, CreatedAt: time.Now().Add(-10 * time.Second)},
+				{ID: 1, Todo: "Tarefa 1", Done: false, CreatedAt: createdAt},
+				{ID: 2, Todo: "Tarefa 2", Done: false, CreatedAt: createdAt},
 			},
 			removedTask: "",
 			idToRemove:  100,
@@ -107,11 +109,11 @@ func TestRemove(t *testing.T) {
 		{
 			name: "Remover uma task com um id válido",
 			tasks: []Task{
-				{ID: 1, Todo: "Tarefa 1", Done: false, CreatedAt: time.Now().Add(-10 * time.Second)},
-				{ID: 2, Todo: "Tarefa 2", Done: false, CreatedAt: time.Now().Add(-10 * time.Second)},
+				{ID: 1, Todo: "Tarefa 1", Done: false, CreatedAt: createdAt},
+				{ID: 2, Todo: "Tarefa 2", Done: false, CreatedAt: createdAt},
 			},
 			remainingTasks: []Task{
-				{ID: 1, Todo: "Tarefa 1", Done: false, CreatedAt: time.Now().Add(-10 * time.Second)},
+				{ID: 1, Todo: "Tarefa 1", Done: false, CreatedAt: createdAt},
 			},
 			removedTask: "Tarefa 2",
 			idToRemove:  2,
@@ -124,19 +126,76 @@ func TestRemove(t *testing.T) {
 			remainingTasks, removedTask, err := Remove(tt.tasks, tt.idToRemove)
 
 			if (err != nil) != tt.wantErr {
-				t.Logf("expected error - %v, got %v: %v", tt.wantErr, err != nil, err)
+				t.Errorf("expected error - %v, got %v: %v", tt.wantErr, err != nil, err)
 			}
-			
+
 			if tt.wantErr {
 				return
 			}
 
-			if slices.Equal(tt.remainingTasks, remainingTasks) {
-				t.Logf("expected slice: %v, got %v", tt.wantErr, err != nil)
+			if !slices.Equal(tt.remainingTasks, remainingTasks) {
+				t.Errorf("expected slice: %#v, got %#v", tt.remainingTasks, remainingTasks)
 			}
 
 			if tt.removedTask != removedTask {
-				t.Logf("expected removed task: %s, got %s", tt.removedTask, removedTask)
+				t.Errorf("expected removed task: %s, got %s", tt.removedTask, removedTask)
+			}
+		})
+	}
+}
+
+func TestMarkAsDone(t *testing.T) {
+	createdAt := time.Now().Add(-10 * time.Second)
+
+	tests := []struct {
+		name     string
+		tasks    []Task
+		idToMark int
+		wantErr  bool
+	}{
+		{
+			name:     "Marcar como concluído uma task em um slice vazio",
+			tasks:    []Task{},
+			idToMark: 1,
+			wantErr:  true,
+		},
+		{
+			name: "Marcar uma task como conclúida com um id inválido",
+			tasks: []Task{
+				{ID: 1, Todo: "Tarefa 1", Done: false, CreatedAt: createdAt},
+				{ID: 2, Todo: "Tarefa 2", Done: false, CreatedAt: createdAt},
+			},
+			idToMark: 100,
+			wantErr:  true,
+		},
+		{
+			name: "Marcar uma task como conclúida com um id válido",
+			tasks: []Task{
+				{ID: 1, Todo: "Tarefa 1", Done: false, CreatedAt: createdAt},
+				{ID: 2, Todo: "Tarefa 2", Done: false, CreatedAt: createdAt},
+			},
+			idToMark: 1,
+			wantErr:  false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tasks, err := MarkAsDone(tt.tasks, tt.idToMark)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("expected erro: %v, got %v - %v", tt.wantErr, err != nil, err)
+			}
+
+			if tt.wantErr {
+				return
+			}
+
+			for i := range tasks {
+				if tasks[i].ID == tt.idToMark && tasks[i].Done == false {
+					t.Errorf("expected the task marked as done it, got not done")
+					return
+				}
 			}
 		})
 	}
